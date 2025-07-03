d37db057d7a6d0ae9c47f76b31fa06b2e060a42e
Nathan Walker <nathanwalker1208@gmail.com>
2025-07-02 20:02:23 -0600

    Implement various other helper functions in Go

--- go/mc-username-override.go
+++ go/mc-username-override.go
@@ -45,3 +45,61 @@ func main() {
 	restartProcess(process, modifiedCmdline)
 	log.Println("Done! Minecraft may take a moment to finish opening.")
 }
+
+func findMinecraftProcess() *process.Process {
+	processes, err := process.Processes()
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+
+	for _, process := range processes {
+		name, err := process.Name()
+		if err != nil {
+			continue
+		}
+		if strings.ToLower(name) == "javaw.exe" {
+			cmdline, err := process.CmdlineSlice()
+			if err != nil {
+				continue
+			}
+			if containsMainClass(cmdline) {
+				return process
+			}
+		}
+	}
+	return nil
+}
+
+func containsMainClass(cmdline []string) bool {
+	mainClasses := []string{
+		"net.minecraft.client.main.Main",
+		"cpw.mods.bootstraplauncher.BootstrapLauncher",
+		"net.fabricmc.loader.impl.launch.knot.KnotClient",
+	}
+	for _, mainClass := range mainClasses {
+		for _, arg := range cmdline {
+			if arg == mainClass {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func inputUsername() string {
+	fmt.Print("Enter your fake username: ")
+	var username string
+	fmt.Scanln(&username)
+	return username
+}
+
+func setUsername(cmdline []string, username string) []string {
+	for i, arg := range cmdline {
+		if arg == "--username" {
+			cmdline[i+1] = username
+			return cmdline
+		}
+	}
+	return nil
+}
